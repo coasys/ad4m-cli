585c283ea8f0f0fe3cd3ce0a8739614ff1066d2f
Jacob Dawid <jada@posteo.de>
2021-07-12 18:18:35 +0200

    Add server command that runs the executor

--- src/cli.js
+++ src/cli.js
@@ -15,7 +15,12 @@ import WebSockets from "ws";
 
 // Perspectivism imports
 import { Ad4mClient } from "../../ad4m";
-//import { Ad4mExecutor } from "../../ad4m-executor";
+import Ad4mExecutor from "../../ad4m-executor";
+
+// Utilities
+import getAppDataPath from "appdata-path";
+import fs from 'fs';
+import path from 'path';
 
 function apolloClient(uri) {
   return new ApolloClient({
@@ -42,6 +47,63 @@ function ad4mClient(uri) {
 
 function queryPassphrase() {
   return "test";
+}
+
+function serveAd4mExecutor() {
+  const worldLinkLanguageHash = 'QmchPr6NgxFUrrETHrd49DSRdfFMdn6A5sw2JSXhujy4gS'
+  const bootstrapPath = path.join(__dirname, '../bootstrap');
+
+  let bootstrapFixtures = {
+    worldPerspective: JSON.parse(fs.readFileSync(path.join(bootstrapPath, 'world.perspective.json'))),
+    worldLinkLanguageHash,
+    worldLinkLinguageBundle:  fs.readFileSync(path.join(bootstrapPath, worldLinkLanguageHash, 'bundle.js')),
+    worldLinkLinguageMeta: JSON.parse(fs.readFileSync(path.join(bootstrapPath, worldLinkLanguageHash, 'meta.json'))),
+  }
+
+  Ad4mExecutor
+  .init({
+    appDataPath: getAppDataPath(),
+    resourcePath: __dirname,
+    appDefaultLangPath: "./src/languages",
+    ad4mBootstrapLanguages: {
+      agents: "agent-profiles",
+      languages: "languages",
+      perspectives: "shared-perspectives"
+    },
+    ad4mBootstrapFixtures: {
+      languages: [{
+        address: bootstrapFixtures.worldLinkLanguageHash,
+        meta: bootstrapFixtures.worldLinkLinguageMeta,
+        bundle: bootstrapFixtures.worldLinkLinguageBundle
+      }],
+      perspectives: [{
+        address: '__world',
+        expression: bootstrapFixtures.worldPerspective
+      }]
+    },
+    appBuiltInLangs: [
+      "social-context",
+      "note-ipfs"
+    ],
+    mocks: false
+  })
+  .then((ad4mCore) => {
+    ad4mCore.waitForAgent().then(async () => {
+      console.log(
+        "\x1b[36m%s\x1b[0m",
+        "Agent has been init'd. Controllers now starting init..."
+      );
+      ad4mCore.initControllers();
+      console.log("\x1b[32m", "Controllers init complete!");
+
+      console.log(
+        "\x1b[36m%s\x1b[0m",
+        "Initializing languages..."
+      );
+      await ad4mCore.initLanguages()
+      console.log("\x1b[32m", "All languages initialized!");
+    });
+  });
 }
 
 export function cli(args) {
