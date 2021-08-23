688283b949d34650ce250df22adeb621cbec35ea
Nicolas Luck <nicolas@lucksus.eu>
2021-08-24 00:33:29 +0200

    Update everything (languages, ad4m-executor, HC) to latest versions

--- scripts/get-builtin-langs.js
+++ scripts/get-builtin-langs.js
@@ -1,27 +1,30 @@
 const fs = require("fs-extra");
 const wget = require("node-wget-js");
+const unzipper = require("unzipper");
+const path = require("path");
 
 const languages = {
   "agent-expression-store": {
     targetDnaName: "agent-store",
-    dna: "https://github.com/perspect3vism/agent-language/releases/download/0.0.3/agent-store.dna",
+    dna: "https://github.com/perspect3vism/agent-language/releases/download/0.0.4/agent-store.dna",
     bundle:
-      "https://github.com/perspect3vism/agent-language/releases/download/0.0.3/bundle.js",
+      "https://github.com/perspect3vism/agent-language/releases/download/0.0.4/bundle.js",
   },
   languages: {
       targetDnaName: "languages",
-      dna: "https://github.com/perspect3vism/language-persistence/releases/download/0.0.1/languages.dna",
-      bundle: "https://github.com/perspect3vism/language-persistence/releases/download/0.0.1/bundle.js",
+      dna: "https://github.com/perspect3vism/language-persistence/releases/download/0.0.3/languages.dna",
+      bundle: "https://github.com/perspect3vism/language-persistence/releases/download/0.0.3/bundle.js",
   },
   "neighbourhood-store": {
     targetDnaName: "neighbourhood-store",
-    dna: "https://github.com/perspect3vism/neighbourhood-language/releases/download/0.0.1/neighbourhood-store.dna",
-    bundle: "https://github.com/perspect3vism/neighbourhood-language/releases/download/0.0.1/bundle.js",
+    //dna: "https://github.com/perspect3vism/neighbourhood-language/releases/download/0.0.1/neighbourhood-store.dna",
+    bundle: "https://github.com/perspect3vism/neighbourhood-language/releases/download/0.0.3/bundle.js",
   },
   "social-context": {
+    zipped: true,
     targetDnaName: "social-context",
-    dna: "https://github.com/juntofoundation/Social-Context/releases/download/0.0.6/social-context.dna",
-    bundle: "https://github.com/juntofoundation/Social-Context/releases/download/0.0.6/bundle.js",
+    resource:
+      "https://github.com/juntofoundation/Social-Context/releases/download/0.0.12/full_index.zip",
   },
   "note-ipfs": {
     bundle: "https://github.com/perspect3vism/lang-note-ipfs/releases/download/0.0.1/bundle.js",
@@ -45,6 +48,33 @@ async function main() {
       url = languages[lang].dna;
       dest = dir + `/${languages[lang].targetDnaName}.dna`;
       wget({ url, dest });
+    }
+
+    if (languages[lang].zipped) {
+      await wget(
+        {
+          url: languages[lang].resource,
+          dest: `${dir}/lang.zip`,
+        },
+        async () => {
+          //Read the zip file into a temp directory
+          await fs
+            .createReadStream(`${dir}/lang.zip`)
+            .pipe(unzipper.Extract({ path: `${dir}` }))
+            .promise();
+
+          // if (!fs.pathExistsSync(`${dir}/bundle.js`)) {
+          //   throw Error("Did not find bundle file in unzipped path");
+          // }
+
+          fs.copyFileSync(
+            path.join(__dirname, `../${dir}/bundle.js`),
+            path.join(__dirname, `../${dir}/build/bundle.js`)
+          );
+          fs.rmSync(`${dir}/lang.zip`);
+          fs.rmSync(`${dir}/bundle.js`);
+        }
+      );
     }
   }
 }
