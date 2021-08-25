5e7cc0fce7eafc634a111a6f51e6f2b571876450
Nicolas Luck <nicolas@lucksus.eu>
2021-08-25 21:16:43 +0200

    Remove developer-setup.sh script since this is done via postinstall

--- developer-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# System requirements/recommendations
-# node through nvm: https://github.com/nvm-sh/nvm (required)
-# nixos: https://nixos.org/download.html (required)
-# cachix: https://app.cachix.org/cache/perspect3vism (optional)
-
-npm install
-npm run postinstall
